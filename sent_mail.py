68dcb49d6efefb42d7cc7e1e0aa67ac591f128c4
ASHUTOSH SWAIN <ashutoshswain465@gmail.com>
2024-12-03 21:32:22 +0530

    Saved password to environment variable

--- sent_mail.py
+++ sent_mail.py
@@ -1,11 +1,12 @@
 import smtplib, ssl
+import os
 
 
 def sent_mail(message):
     host = "smtp.gmail.com"
     port = 465
     username = "ashutoshswain465@gmail.com"
-    password = "hwvr hddd qnhe nmah"
+    password = os.getenv("ST01_PASS")
     receiver = "ashutoshswain465@gmail.com"
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
